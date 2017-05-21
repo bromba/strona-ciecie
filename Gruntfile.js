bbf768c671465b34e407f5283d5aafd35c047667
bromba <bromba1@tlen.pl>
2017-05-21 20:46:22 +0200

    automatization for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,10 +11,31 @@ module.exports = function(grunt) {
          'css/main.css': 'sass/main.sass'
         }
       }
+    },
+    imagemin: {
+    dynamic: {
+        files: [{
+            expand: true,
+            cwd: 'images/',
+            src: ['**/*.{png,jpg,gif}'],
+            dest: 'images/build/'
+        }]
     }
+},
+watch: {
+    scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+            spawn: false,
+        }
+    } 
+},
   });
   // Load the plugins tasks 
   grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   // Default task(s).
-  grunt.registerTask('default', ["sass"]);
+  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
 };
