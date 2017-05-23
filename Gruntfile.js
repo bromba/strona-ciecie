c0609f14c1bd4b4f57070ef551026fd556aabbd2
bromba <bromba1@tlen.pl>
2017-05-23 20:05:55 +0200

    gruntfile modification for picture conversion. new files with converted ictures

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,7 +18,7 @@ module.exports = function(grunt) {
             expand: true,
             cwd: 'images/',
             src: ['**/*.{png,jpg,gif}'],
-            dest: 'images/build/'
+            dest: 'conversed/'
         }]
     }
 },
